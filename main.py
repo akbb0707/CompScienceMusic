c83ea393e992a21ca9ac1c6e93ae67b8c20385dd
akbb0707 <62500386+akbb0707@users.noreply.github.com>
2021-12-08 21:06:55 +0000

    Updated version
    Properly saves scores to file now
    Prints out everyone's scores at the end

--- main.py
+++ main.py
@@ -43,8 +43,7 @@ while True:
                 break
             addingUser(toAppendName)
     elif choice == "3":
-        print("Coming Soon!")
-        continue
+        break
     elif choice == "done":
         break
     elif choice == "exit":
@@ -57,11 +56,11 @@ while True:
 try:
     tempSongs = open("music.txt", encoding="utf-8").readlines()
 except FileNotFoundError:
-    print("Music file not found! Please run the addingMusic script.")
+    print("Music file not found! Please run the 1st choice on the main menu.")
 try:
     tempPlayers = open("players.txt", encoding="utf-8").readlines()
 except FileNotFoundError:
-    print("No users file found! Please run the addingUser script.")
+    print("No users file found! Please run the 2nd option on the main menu.")
     exit(0)
 songs = []
 artists = []
@@ -73,40 +72,59 @@ for element in tempSongs:
 for element1 in tempPlayers:
     players.append(element1.strip())
 
+
 for iteration, titleAndSong in enumerate(songs):
     try:
         title, artist = titleAndSong.split(", ")
         artists.append(artist)
         songs[iteration] = title
-        print(title, artist)
     except ValueError:
         print(f"\033[1mERROR:\033[0m Invalid song file syntax detected at line: \033[1m{int(iteration) + 1}\033[0m of music.txt file")
 
 for iteration1, playerItem in enumerate(players):
     try:
         player, score = playerItem.split(" ")
-        print(player, score)
         players[iteration1] = player
         scores.append(score)
     except ValueError:
         print(f"\033[1mERROR:\033[0m Invalid player file syntax detected at line: \033[1m{int(iteration1) + 1}\033[0m of players.txt file")
 
 
-print(scores)
-print(players)
+# Actual game
 
-for z in range(5):
+for users in range(len(scores)):
+  print(f"\nPlayer {players[users]} is playing now\n")
+  for z in range(2):
     random_choice = random.randrange(len(songs))
     random_artist = artists[random_choice]
     random_song = songs[random_choice]
     print(f"{random_artist} - {random_song[0]}")
     guess1 = input("Enter the first guess for the song: ")
     if guess1 == str(random_song):
-        print(scores[0])
-        oldScore = scores.pop(0)
-        scores[0].append(oldScore + 3)
+        tempScore = int(scores[users])
+        tempScore += 3
+        scores[users] = str(tempScore)       
     elif guess1 != str(random_song):
         guess2 = input("One more chance: ")
         if guess2 == random_song:
-            print(scores[0])
-            scores[0] += 1
+            tempScore = int(scores[users])
+            tempScore += 1
+            scores[users] = str(tempScore)
+
+openFile1 = open("players.txt", "a", encoding="utf-8")
+openFile1.truncate(0)
+
+for g in range(len(scores)):
+    print(f"{players[g]} got a score of {scores[g]}")
+
+for u in range(len(scores)):
+    with open("players.txt", "a+") as file_object2:
+      # Move read cursor to the start of file.
+      file_object2.seek(0)
+      # If file is not empty then append '\n'
+      data = file_object2.read(100)
+      if len(data) > 0 :
+          file_object2.write("\n")
+      # Append text at the end of file
+      file_object2.write(f"{players[u]} {scores[u]}")
+      file_object2.close()
